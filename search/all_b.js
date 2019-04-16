549d8d22acc813949b320401c0df31e4a46bd7da
CI <c-sdk@newrelic.com>
2019-04-16 04:19:26 +0000

    Update documentation

--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['query',['query',['../struct__newrelic__datastore__segment__params__t.html#aca1e816f53e52957918f5665da0ec248',1,'_newrelic_datastore_segment_params_t']]]
+  ['record_5fsql',['record_sql',['../structnewrelic__transaction__tracer__config__t.html#ac872a00f98d185c379b2d537352a52dd',1,'newrelic_transaction_tracer_config_t']]],
+  ['redirect_5fcollector',['redirect_collector',['../structnewrelic__app__config__t.html#a5ddc14dbc80c8254e0f24739282db90f',1,'newrelic_app_config_t']]]
 ];
--- search/enums_0.js
+++ search/enums_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['_5fnewrelic_5floglevel_5ft',['_newrelic_loglevel_t',['../libnewrelic_8h.html#a23ebe265c8d9e437355c21caf81d6dec',1,'libnewrelic.h']]],
-  ['_5fnewrelic_5ftransaction_5ftracer_5fthreshold_5ft',['_newrelic_transaction_tracer_threshold_t',['../libnewrelic_8h.html#a9155550fbe3e64567b6b1cc18e71f2f3',1,'libnewrelic.h']]],
-  ['_5fnewrelic_5ftt_5frecordsql_5ft',['_newrelic_tt_recordsql_t',['../libnewrelic_8h.html#a96e80fea6e99150a0c1cf8947afac2f2',1,'libnewrelic.h']]]
+  ['newrelic_5floglevel_5ft',['newrelic_loglevel_t',['../libnewrelic_8h.html#af01e9a01f02a47a22714224bb8357619',1,'libnewrelic.h']]],
+  ['newrelic_5ftransaction_5ftracer_5fthreshold_5ft',['newrelic_transaction_tracer_threshold_t',['../libnewrelic_8h.html#a935c8c50ee8d9ce8e0e652182918fac3',1,'libnewrelic.h']]],
+  ['newrelic_5ftt_5frecordsql_5ft',['newrelic_tt_recordsql_t',['../libnewrelic_8h.html#adc4633517354b6ee5c04e7b3585b3140',1,'libnewrelic.h']]]
 ];
