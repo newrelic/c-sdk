c660f424973b8bda72af331604defe0a926cb883
CI <c-sdk@newrelic.com>
2019-04-16 17:03:24 +0000

    Update documentation

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['record_5fsql',['record_sql',['../struct__newrelic__transaction__tracer__config__t.html#a49a5ca796e2c651cbc2b4b5b9e2a8465',1,'_newrelic_transaction_tracer_config_t']]],
-  ['redirect_5fcollector',['redirect_collector',['../struct__newrelic__app__config__t.html#a074eb63e5f26b2b7351e937d7acced7a',1,'_newrelic_app_config_t']]]
+  ['record_5fsql',['record_sql',['../structnewrelic__transaction__tracer__config__t.html#ac872a00f98d185c379b2d537352a52dd',1,'newrelic_transaction_tracer_config_t']]],
+  ['redirect_5fcollector',['redirect_collector',['../structnewrelic__app__config__t.html#a5ddc14dbc80c8254e0f24739282db90f',1,'newrelic_app_config_t']]]
 ];
