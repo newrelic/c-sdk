242ea8448a5157adf42428fd2ba00f8af063f775
CI <c-sdk@newrelic.com>
2019-04-16 14:12:23 +0000

    Update documentation

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['newrelic_5flog_5fdebug',['NEWRELIC_LOG_DEBUG',['../libnewrelic_8h.html#a23ebe265c8d9e437355c21caf81d6deca904a67414971f81a6f9efc13d5b6977e',1,'libnewrelic.h']]],
-  ['newrelic_5flog_5ferror',['NEWRELIC_LOG_ERROR',['../libnewrelic_8h.html#a23ebe265c8d9e437355c21caf81d6decab882c950a757e52dfaab9ba359f97a7a',1,'libnewrelic.h']]],
-  ['newrelic_5flog_5finfo',['NEWRELIC_LOG_INFO',['../libnewrelic_8h.html#a23ebe265c8d9e437355c21caf81d6decaa179b20d36868851d1b29b22e4c01624',1,'libnewrelic.h']]],
-  ['newrelic_5flog_5fwarning',['NEWRELIC_LOG_WARNING',['../libnewrelic_8h.html#a23ebe265c8d9e437355c21caf81d6deca9e59320b072b1035ce19a1f14909ba88',1,'libnewrelic.h']]],
-  ['newrelic_5fsql_5fobfuscated',['NEWRELIC_SQL_OBFUSCATED',['../libnewrelic_8h.html#a96e80fea6e99150a0c1cf8947afac2f2a27572e656df9266fdf8aea6c2e46a4bd',1,'libnewrelic.h']]],
-  ['newrelic_5fsql_5foff',['NEWRELIC_SQL_OFF',['../libnewrelic_8h.html#a96e80fea6e99150a0c1cf8947afac2f2a3ad3f3f914f745ace66af39a5fe025d8',1,'libnewrelic.h']]],
-  ['newrelic_5fsql_5fraw',['NEWRELIC_SQL_RAW',['../libnewrelic_8h.html#a96e80fea6e99150a0c1cf8947afac2f2acdbc5693404f3319183d89b143857c10',1,'libnewrelic.h']]],
-  ['newrelic_5fthreshold_5fis_5fapdex_5ffailing',['NEWRELIC_THRESHOLD_IS_APDEX_FAILING',['../libnewrelic_8h.html#a9155550fbe3e64567b6b1cc18e71f2f3a18c3e6b289993346c327bdcbcf7359b4',1,'libnewrelic.h']]],
-  ['newrelic_5fthreshold_5fis_5fover_5fduration',['NEWRELIC_THRESHOLD_IS_OVER_DURATION',['../libnewrelic_8h.html#a9155550fbe3e64567b6b1cc18e71f2f3a195c5351f0ebc41e00c110cd64eb3cac',1,'libnewrelic.h']]]
+  ['newrelic_5flog_5fdebug',['NEWRELIC_LOG_DEBUG',['../libnewrelic_8h.html#af01e9a01f02a47a22714224bb8357619a904a67414971f81a6f9efc13d5b6977e',1,'libnewrelic.h']]],
+  ['newrelic_5flog_5ferror',['NEWRELIC_LOG_ERROR',['../libnewrelic_8h.html#af01e9a01f02a47a22714224bb8357619ab882c950a757e52dfaab9ba359f97a7a',1,'libnewrelic.h']]],
+  ['newrelic_5flog_5finfo',['NEWRELIC_LOG_INFO',['../libnewrelic_8h.html#af01e9a01f02a47a22714224bb8357619aa179b20d36868851d1b29b22e4c01624',1,'libnewrelic.h']]],
+  ['newrelic_5flog_5fwarning',['NEWRELIC_LOG_WARNING',['../libnewrelic_8h.html#af01e9a01f02a47a22714224bb8357619a9e59320b072b1035ce19a1f14909ba88',1,'libnewrelic.h']]],
+  ['newrelic_5fsql_5fobfuscated',['NEWRELIC_SQL_OBFUSCATED',['../libnewrelic_8h.html#adc4633517354b6ee5c04e7b3585b3140a27572e656df9266fdf8aea6c2e46a4bd',1,'libnewrelic.h']]],
+  ['newrelic_5fsql_5foff',['NEWRELIC_SQL_OFF',['../libnewrelic_8h.html#adc4633517354b6ee5c04e7b3585b3140a3ad3f3f914f745ace66af39a5fe025d8',1,'libnewrelic.h']]],
+  ['newrelic_5fsql_5fraw',['NEWRELIC_SQL_RAW',['../libnewrelic_8h.html#adc4633517354b6ee5c04e7b3585b3140acdbc5693404f3319183d89b143857c10',1,'libnewrelic.h']]],
+  ['newrelic_5fthreshold_5fis_5fapdex_5ffailing',['NEWRELIC_THRESHOLD_IS_APDEX_FAILING',['../libnewrelic_8h.html#a935c8c50ee8d9ce8e0e652182918fac3a18c3e6b289993346c327bdcbcf7359b4',1,'libnewrelic.h']]],
+  ['newrelic_5fthreshold_5fis_5fover_5fduration',['NEWRELIC_THRESHOLD_IS_OVER_DURATION',['../libnewrelic_8h.html#a935c8c50ee8d9ce8e0e652182918fac3a195c5351f0ebc41e00c110cd64eb3cac',1,'libnewrelic.h']]]
 ];
