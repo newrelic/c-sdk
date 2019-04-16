2dd98ae28954d383809fe74e0a8d32fbee46dc26
CI <c-sdk@newrelic.com>
2019-04-16 13:53:32 +0000

    Update documentation

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['instance_5freporting',['instance_reporting',['../structnewrelic__datastore__segment__config__t.html#a50686f3b262b03e216e0b08b63872c4e',1,'newrelic_datastore_segment_config_t']]]
+  ['host',['host',['../struct__newrelic__datastore__segment__params__t.html#ae82c1196e2ce16590ab7ac39f208a0d2',1,'_newrelic_datastore_segment_params_t']]]
 ];
