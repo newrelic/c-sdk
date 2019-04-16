98332866fb59e85d068ccb6871a8c52d74c8651a
CI <c-sdk@newrelic.com>
2019-04-16 13:36:28 +0000

    Update documentation

--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['instance_5freporting',['instance_reporting',['../struct__newrelic__datastore__segment__config__t.html#adda209b616bc23655104be2dc3ca8e6d',1,'_newrelic_datastore_segment_config_t']]]
+  ['libnewrelic_2eh',['libnewrelic.h',['../libnewrelic_8h.html',1,'']]],
+  ['library',['library',['../structnewrelic__external__segment__params__t.html#a128aedbfca7e8eebd3a38d0366d94f33',1,'newrelic_external_segment_params_t']]],
+  ['license_5fkey',['license_key',['../structnewrelic__app__config__t.html#a84a45cf18786e4773a6264abfd4a330c',1,'newrelic_app_config_t']]],
+  ['log_5ffilename',['log_filename',['../structnewrelic__app__config__t.html#a7723894306f47dab64bbea6ba50bfa8d',1,'newrelic_app_config_t']]],
+  ['log_5flevel',['log_level',['../structnewrelic__app__config__t.html#aa8cdeedde66ede25d4f3083e6e2c24b2',1,'newrelic_app_config_t']]]
 ];
