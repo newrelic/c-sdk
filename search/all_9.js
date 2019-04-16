7d05ee851df319f199dbe96df3feb3035c939795
CI <c-sdk@newrelic.com>
2019-04-16 12:50:30 +0000

    Update documentation

--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['port_5fpath_5for_5fid',['port_path_or_id',['../structnewrelic__datastore__segment__params__t.html#ada559d43f36aea5fa2eaa54f8ba3ae14',1,'newrelic_datastore_segment_params_t']]],
-  ['procedure',['procedure',['../structnewrelic__external__segment__params__t.html#abbac82d5d9518b42c535dda84162aa25',1,'newrelic_external_segment_params_t']]],
-  ['product',['product',['../structnewrelic__datastore__segment__params__t.html#acdd3a868d55331c4f135dd2f4d003a20',1,'newrelic_datastore_segment_params_t']]]
+  ['operation',['operation',['../struct__newrelic__datastore__segment__params__t.html#a7ff80afeae4d2014f1437d5a5bf7b653',1,'_newrelic_datastore_segment_params_t']]]
 ];
--- search/variables_8.js
+++ search/variables_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['port_5fpath_5for_5fid',['port_path_or_id',['../structnewrelic__datastore__segment__params__t.html#ada559d43f36aea5fa2eaa54f8ba3ae14',1,'newrelic_datastore_segment_params_t']]],
-  ['procedure',['procedure',['../structnewrelic__external__segment__params__t.html#abbac82d5d9518b42c535dda84162aa25',1,'newrelic_external_segment_params_t']]],
-  ['product',['product',['../structnewrelic__datastore__segment__params__t.html#acdd3a868d55331c4f135dd2f4d003a20',1,'newrelic_datastore_segment_params_t']]]
+  ['port_5fpath_5for_5fid',['port_path_or_id',['../struct__newrelic__datastore__segment__params__t.html#ac07e447ccf883e8f65477748a9f00d65',1,'_newrelic_datastore_segment_params_t']]],
+  ['procedure',['procedure',['../struct__newrelic__external__segment__params__t.html#a5f139b8e758ff5110050b7828890d623',1,'_newrelic_external_segment_params_t']]],
+  ['product',['product',['../struct__newrelic__datastore__segment__params__t.html#a90e39403746c278390f5834a4771c96e',1,'_newrelic_datastore_segment_params_t']]]
 ];
