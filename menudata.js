9c261b1348eac8b4991206a902e4582687917ed6
CI <c-sdk@newrelic.com>
2019-04-17 02:07:46 +0000

    Update documentation

--- menudata.js
+++ menudata.js
@@ -32,11 +32,10 @@ var menudata={children:[
 {text:"File List",url:"files.html"},
 {text:"Globals",url:"globals.html",children:[
 {text:"All",url:"globals.html",children:[
-{text:"_",url:"globals.html#index__5F"},
 {text:"n",url:"globals.html#index_n"}]},
 {text:"Functions",url:"globals_func.html",children:[
 {text:"n",url:"globals_func.html#index_n"}]},
 {text:"Typedefs",url:"globals_type.html"},
 {text:"Enumerations",url:"globals_enum.html"},
-{text:"Enumerator",url:"globals_eval.html"},
-{text:"Macros",url:"globals_defs.html"}]}]}]}
+{text:"Enumerator",url:"globals_eval.html"}]}]},
+{text:"Examples",url:"examples.html"}]}
