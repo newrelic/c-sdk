e20465452f5bd92d999cef5fcc29dc03a63f8ba5
Adam Harvey <adam@adamharvey.name>
2020-01-03 13:42:15 -0800

    Update to handle the app timeout logic moving into axiom.

--- src/app_internal.c
+++ src/app_internal.c
@@ -13,9 +13,6 @@
 nr_status_t newrelic_connect_app(newrelic_app_t* app,
                                  unsigned short timeout_ms) {
   nrapp_t* nrapp;
-  const int retry_sleep_ms = 50;
-  nrtime_t start_time;
-  nrtime_t delta_time;
   nrtime_t timeout_time = timeout_ms * NR_TIME_DIVISOR_MS;
 
   if (NULL == app) {
@@ -28,19 +25,8 @@ nr_status_t newrelic_connect_app(newrelic_app_t* app,
     return NR_FAILURE;
   }
 
-  // Query the daemon until a successful connection is made or timeout occurs.
-  start_time = nr_get_time();
-  while (true) {
-    nrapp = nr_agent_find_or_add_app(nr_agent_applist, app->app_info, NULL);
-
-    delta_time = nr_time_duration(start_time, nr_get_time());
-    if (NULL != nrapp || (delta_time >= timeout_time)) {
-      break;
-    }
-
-    nr_msleep(retry_sleep_ms);
-  };
-
+  nrapp = nr_agent_find_or_add_app(nr_agent_applist, app->app_info, NULL,
+                                   timeout_time);
   if (NULL == nrapp) {
     nrl_error(NRL_INSTRUMENT, "application was unable to connect");
     return NR_FAILURE;
